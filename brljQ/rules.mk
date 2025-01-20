898c59314ab7762ffb35952a8efbb05fa3bfcbb7
The Potato Man from The Potato Church <potato.man@thepotatochurch.com>
2025-01-20 21:07:44 +0000

    fix: tweak touchpad config

--- brljQ/rules.mk
+++ brljQ/rules.mk
@@ -1,6 +1,6 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-CONSOLE_ENABLE = no
+CONSOLE_ENABLE = yes
 COMMAND_ENABLE = no
 AUTO_SHIFT_ENABLE = yes
 ORYX_ENABLE = yes
@@ -8,5 +8,7 @@ RGB_MATRIX_CUSTOM_KB = yes
 TAP_DANCE_ENABLE = yes
 SPACE_CADET_ENABLE = no
 CAPS_WORD_ENABLE = yes
+
+I2C_DRIVER_REQUIRED = yes
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
